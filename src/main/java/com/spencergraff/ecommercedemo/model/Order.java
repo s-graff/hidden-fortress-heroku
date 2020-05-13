00442ab897f4fb88b70cef5c64084af0efda31ed
Spencer Graff <948056+s-graff@users.noreply.github.com>
2020-05-13 14:20:35 -0600

    Testing reverting

--- src/main/java/com/spencergraff/ecommercedemo/model/Order.java
+++ src/main/java/com/spencergraff/ecommercedemo/model/Order.java
@@ -14,8 +14,7 @@ import java.util.List;
 @Entity
 @Table(name = "orders")
 @JsonIdentityInfo(generator=ObjectIdGenerators.PropertyGenerator.class, property="orderProducts")
-public class Order
-{
+public class Order {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
